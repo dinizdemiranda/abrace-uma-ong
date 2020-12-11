dc337753970fb1942272f069a129744d3f2d7751
dinizdemiranda <67244187+dinizdemiranda@users.noreply.github.com>
2020-12-11 11:00:53 -0300

    bug de botao quebrando

--- src/Donations.js
+++ src/Donations.js
@@ -34,15 +34,14 @@ const Donations = ({limit}) => {
         <div id="receipts">
             <div className="panel-list-title">
                 <h4>Histórico</h4>
-                <div>
                 <button class="panel-button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
                         <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
                     </svg>
-                    <span>Obter Abracoins</span>
+                    Obter Abracoins
                 </button>    
-                </div>
+                
             </div>            
             <div id="receipts-board">
 
